2a3a246296e67b0d2746bc5a41db3fa675b01b6c
VitekDobrovsky <vit.dobrovsky@seznam.cz>
2023-12-15 09:20:43 +0100

    resize mountains

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('moutnain').style.width = String(window.innerWidth) + 'px'; 
+
+
+window.addEventListener('resize', () => {
+    document.getElementById('moutnain').style.width = String(windwo.innerWidth) + 'px';
+
+});
