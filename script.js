32f75521a56d3085e7d2e5bdcbf58fe90e9f6988
Vítek Dobrovský <vit.dobrovsky@seznam.cz>
2023-12-16 14:00:53 +0100

    cmt

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('moutnain').style.width = String(window.innerWidth) + 'px';
+addEventListener('resize', () => {
+    document.getElementById('moutnain').style.width = String(window.innerWidth) + 'px';
+});
